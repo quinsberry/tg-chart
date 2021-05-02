0f11cdf645bb5dd42ad3b0c177e9fabdd3a9ffde
ekolesnyk <eugene.kolesnyk@customertimes.com>
2021-05-02 20:50:04 +0300

    - changed some dev/build settings

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import { data } from './data'
 import { chart } from './chart'
-import './styles.scss'
 
 const tgChart = chart(document.getElementById('chart') as HTMLCanvasElement, data[0])
 tgChart.init()
